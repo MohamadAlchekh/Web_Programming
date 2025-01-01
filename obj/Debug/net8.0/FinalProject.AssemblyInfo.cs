6720e26b9108a5378614e239c36037f57f9758f8
Mohamad alchekh <m.alchekh@outlook.com>
2025-01-01 22:05:31 +0300

    ---

--- obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1783351228137a40fd9cff4110ecf832c068cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26bebb17fd558ad08a7ae3077c9fae6d8afaac46")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
