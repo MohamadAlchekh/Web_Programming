26bebb17fd558ad08a7ae3077c9fae6d8afaac46
Mohamad alchekh <m.alchekh@outlook.com>
2025-01-01 22:02:35 +0300

    --

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,45 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using FinalProject.Models;
-using System;
-using System.Collections.Generic;
 
 namespace FinalProject.Controllers
 {
     public class ProfileController : Controller
     {
-        public IActionResult Index(string username)
-        {
-            // Şimdilik örnek veri oluşturalım
-            var userProfile = new UserProfile
-            {
-                Id = 1,
-                Name = "Test Kullanıcı",
-                Username = username ?? "testuser",
-                Location = "İstanbul",
-                JoinDate = DateTime.Now.AddMonths(-3),
-                ActivityCount = 5,
-                FollowersCount = 120,
-                FollowingCount = 85,
-                Activities = new List<Activity>
-                {
-                    new Activity 
-                    { 
-                        Id = 1, 
-                        Name = "Yazılım Geliştirme Konferansı", 
-                        Type = "Etkinlik", 
-                        Date = DateTime.Now.AddDays(-5) 
-                    },
-                    new Activity 
-                    { 
-                        Id = 2, 
-                        Name = "UX/UI Workshop", 
-                        Type = "Workshop", 
-                        Date = DateTime.Now.AddDays(-2) 
-                    }
-                }
-            };
+        private readonly DBContext _context;
 
-            return View(userProfile);
+        public ProfileController(DBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            if (_context.Users == null)
+            {
+                return NotFound();
+            }
+            var user = _context.Users.FirstOrDefault();
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
         }
     }
 } 
