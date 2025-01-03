d0566d9c055eb5c52bc9caa495be0bad136897e7
Mohamad alchekh <m.alchekh@outlook.com>
2025-01-03 13:12:30 +0300

    Login ve signup Yapildi

---
+++ Models/Repositories/Repository.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalProject.Models.Repositories
+{
+    public static class Repository
+    {
+        public static List<User> Kullanici_Bilgileri = new List<User>();
+        public static void Ekle(User model)
+        {
+            Kullanici_Bilgileri.Add(model);
+        }
+    }
+}
