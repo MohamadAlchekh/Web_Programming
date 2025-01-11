bd00dfeb6e5aa9a99e47919e90ca0e6bdc973a64
Mohamad alchekh <m.alchekh@outlook.com>
2025-01-11 15:26:32 +0300

    before merging

--- obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c19b356e23bf9faa770625d5509b448995965f18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ca43cd0350fafc860dda3cbc2ff264f83d8fd56")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
